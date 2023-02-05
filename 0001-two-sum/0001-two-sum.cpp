0632f06db16a1a66b0307a8fd0806e60d04b6870
Naveenbasyal <72588078+naveenbasyal@users.noreply.github.com>
2023-02-05 21:32:08 +0530

    Time: 8 ms (95.40%), Space: 11.1 MB (19.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,16 +4,16 @@ public:
         vector<pair<int,int>> ans;
         vector<int> v;
         for(int i=0;i<nums.size();i++){
-            ans.push_back({nums[i],i});
+            ans.push_back({nums[i],i});//storing elts with indexes in vector
         }
         sort(ans.begin(),ans.end());
         int i=0, j=ans.size()-1;
         
-        while(i<j){
+        while(i<j){//doing a left right search
             if(ans[i].first+ans[j].first==target){
                 v.push_back(ans[i].second);
                 v.push_back(ans[j].second);
-                break;//must: because we want the loop to stop ,otherwi
+                break;//must: because we want the loop to stop ,otherwise it will lead to TLE
             }
             else if(ans[i].first + ans[j].first > target) {
                 j--;
