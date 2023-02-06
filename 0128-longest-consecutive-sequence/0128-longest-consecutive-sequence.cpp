f3ac4198b691c7c28b2b97ab9a8d8d6a1abae873
Naveenbasyal <72588078+naveenbasyal@users.noreply.github.com>
2023-02-06 22:27:34 +0530

    Time: 93 ms (86.37%), Space: 33.8 MB (92.60%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,16 +5,16 @@ public:
         if(n==0) return 0;
         if(n==1) return 1;
         sort(nums.begin(),nums.end());
-        int c = 1 , res=1;
+        int count = 1 , ans=1;
         for(int i = 0; i < n - 1; i++){
-            if(nums[i+1]==nums[i]+1){
-                c++;
-                res=max(res,c);
+            if(nums[i]+1 == nums[i+1]){
+                count ++;
+                ans = max(ans,count);
             }
             else if(nums[i]==nums[i+1]) continue;
-            else c=1;
+            else count = 1;
         }
-        return res;
+        return ans;
     }
     
 };
