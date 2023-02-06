f6127c580ecdf9f9dccc37e8c626de10a7bb6a18
Naveenbasyal <72588078+naveenbasyal@users.noreply.github.com>
2023-02-06 22:31:36 +0530

    Time: 58 ms (99.76%), Space: 33.7 MB (98.66%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,14 +5,21 @@ public:
         if(n==0) return 0;
         if(n==1) return 1;
         sort(nums.begin(),nums.end());
-        int count = 1 , ans=1;
+        /*
+            we simply sort the array and compare them .
+            and we store the max length of consecutive elts in the ans.
+            .if the compare is failed we set the length to 1 again.
+            we are taking length 1 because even if there is 1 element in the array 
+            the length will be 1 .
+        */
+        int length = 1 , ans=1;
         for(int i = 0; i < n - 1; i++){
             if(nums[i]+1 == nums[i+1]){
-                count ++;
-                ans = max(ans,count);
+                length ++;
+                ans = max(ans,length);
             }
-            else if(nums[i]==nums[i+1]) continue;
-            else count = 1;
+            else if(nums[i] == nums[i+1]) continue; // is duplicate we continue
+            else length = 1;
         }
         return ans;
     }
