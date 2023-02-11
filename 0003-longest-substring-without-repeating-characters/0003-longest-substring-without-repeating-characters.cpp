e48d2a4b760df08eb92abb474842bbe7f95faf73
Naveenbasyal <72588078+naveenbasyal@users.noreply.github.com>
2023-02-11 13:55:59 +0530

    Time: 39 ms (31.19%), Space: 13.5 MB (14.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        /* Intuition:
+                To find the length of the longest substring without repeating characters in a given string "s",                 you can use the "Sliding Window" technique. Here's the general idea:
+
+                Create two pointers, "start" and "end", initially set to 0.
+                Use a hash set to store the characters in the current substring.
+                While "end" is less than the length of the string:
+                a. If the character at "end" is not in the hash set, add it and
+                     move the "end" pointer to the right.
+                b. If the character at "end" is already in the hash set, remove the
+                    character at "start" from the hash set and move the "start" pointer to the right.
+                c. The length of the longest substring is the maximum distance between the 
+                    "start" and "end" pointers.       
+        */
         int start = 0, end = 0 , len = 0;
         set<int> st;
         while(end < s.size()){
