b3849b0e695e0f9644a10d59edbcb47e7c4d4711
Naveenbasyal <72588078+naveenbasyal@users.noreply.github.com>
2023-02-21 22:15:44 +0530

    Time: 5 ms (6.44%), Space: 6.3 MB (55.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,17 +3,17 @@ public:
     bool isValid(string s) {
         
          stack<char> st;
-    for(char c : s){
-        if(c == '('|| c == '{' || c == '['){
-            st.push(c);
-        }else{
-            if(st.empty()) return false;
-            if(c == ')' && st.top() != '(') return false;
-            if(c == '}' && st.top() != '{') return false;
-            if(c == ']' && st.top() != '[') return false;
-            st.pop();
+        for(char c : s){
+            if(c == '('|| c == '{' || c == '['){
+                st.push(c);
+            }else{
+                if(st.empty() or (c == ']' and st.top() != '[') or
+                   (c == '}' and st.top() != '{') or (c == ')' and st.top() != '(')){
+                    return false;
+                }        
+                st.pop();
+            }
         }
-    }
-    return st.empty();
+        return st.empty();
     }
 };
