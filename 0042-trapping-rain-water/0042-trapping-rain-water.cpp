3b8b010fd3948f832cc209131cdea83a323bb2c9
Naveen Basyal <72588078+naveenbasyal@users.noreply.github.com>
2023-05-04 20:38:43 +0530

    Time: 20 ms (35.50%), Space: 19.8 MB (85.21%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,19 +4,22 @@ public:
         int left[height.size()];
         int right[height.size()];
         
-        int maxi=INT_MIN;
-        for(int i=0;i<height.size();i++){
-            maxi= max(maxi,height[i]);
-            left[i]=maxi;
+        int maxi = INT_MIN;
+        
+        for(int i = 0; i < height.size(); i++){  
+            maxi = max(maxi, height[i]);
+            left[i] = maxi;
         }
-        maxi=INT_MIN;
-        for(int i=height.size()-1;i>=0;i--){
-            maxi= max(maxi,height[i]);
-            right[i]=maxi;
+        maxi = INT_MIN;
+        for(int i = height.size() - 1; i >= 0 ; i--){  
+            maxi = max(maxi, height[i]);
+            right[i] = maxi;
         }
+        
         int ans=0;
+        
         for(int i=0;i<height.size();i++){
-            ans+=min(left[i],right[i])-height[i];
+            ans += min(right[i],left[i]) - height[i];
         }
         return ans;
     }
