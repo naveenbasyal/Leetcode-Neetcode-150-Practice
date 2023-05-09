ac47f24947aa078234fcf95d3d873aa4dce0db79
Naveen Basyal <72588078+naveenbasyal@users.noreply.github.com>
2023-05-09 23:16:59 +0530

    Added solution - LeetHub

---
+++ Introduction to Trees - GFG/introduction-to-trees.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countNodes(int i) {
+        if(i == 1) return 1;
+        int nodes = 1;
+        for(int c = 1; c < i; c++){
+            nodes = nodes * 2;
+        }
+        return nodes;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int i;
+        cin >> i;
+        Solution ob;
+        int res = ob.countNodes(i);
+
+        cout << res;
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
