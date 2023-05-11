4d0a520d6f5138195e70751fc911f3c75a0bba59
Naveen Basyal <72588078+naveenbasyal@users.noreply.github.com>
2023-05-11 15:16:09 +0530

    Time: 35 ms, Memory: 26 MB - LeetHub

--- construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,41 +11,34 @@
  */
 class Solution {
 public:
-    
-    int getPos(vector<int> &inorder , int n , int element){
+    // Brute Force Code
+    int getPos(vector<int>& inorder,int node,int n){
         for(int i = 0; i < n; i++){
-            if(inorder[i] == element){
+            if(inorder[i] == node){
                 return i;
             }
-        }
+        }    
         return -1;
     }
     
-    TreeNode* solve(vector<int>& preorder,vector<int> & inorder,
-                    int &preorder_id,int n , int inorder_left ,int inorder_right)
-    {
+    TreeNode* helper(vector<int>& preorder , vector<int>& inorder, int &id , int inLeft , int inRight , int n){
+        //base 
+        if(id > n || inLeft > inRight) return NULL;
         
-        if(preorder_id > n || inorder_left > inorder_right) return NULL;
+        int node = preorder[id++];
+        TreeNode* root = new TreeNode(node);
+        int pos = getPos(inorder , node , n);
         
-        int element = preorder[preorder_id++];
-        
-        TreeNode* root = new TreeNode(element);
-        
-        int pos = getPos(inorder , n , element);
-        
-        root->left = solve(preorder,inorder,preorder_id , n , inorder_left , pos - 1);
-        root->right = solve(preorder,inorder,preorder_id  , n , pos + 1 , inorder_right);
-        
+        root->left = helper(preorder , inorder , id , inLeft , pos - 1 , n);
+        root->right = helper(preorder , inorder , id , pos + 1 , inRight , n);
         return root;
-        
     }
-    
+        
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n = inorder.size();
-        int preorder_id = 0 , inorder_Left = 0 ,  inorder_Right = n - 1 ;
-       TreeNode* root =  solve(preorder , inorder , preorder_id , n , inorder_Left ,inorder_Right);
+        int n = preorder.size();
+        int id = 0 , inLeft = 0 , inRight = n - 1 ;
         
+        TreeNode* root = helper(preorder , inorder , id , inLeft , inRight , n);
         return root;
-        
     }
 };
