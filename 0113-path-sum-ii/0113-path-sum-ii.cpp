2d4a2b0a2dad0a959b4f5458538721ab31e5645d
Naveen Basyal <72588078+naveenbasyal@users.noreply.github.com>
2023-05-17 19:01:30 +0530

    Time: 15 ms (46.92%), Space: 19.6 MB (99.93%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -12,35 +12,29 @@
 class Solution {
 public:
     
-   void getPath(TreeNode* root, vector<vector<int>>& ans, vector<int>& path, int& sum, int targetSum) {
-    if (root == nullptr)
-        return;
-
-    sum += root->val;
-    path.push_back(root->val);
-
-    if (sum == targetSum && root->left == nullptr && root->right == nullptr) {
-        ans.push_back(path);
+    void getPath(TreeNode* root,vector<vector<int>> &ans ,vector<int> &path,int &sum, int targetSum){
+        if(root == NULL) return;
+        sum += root->val;
+        path.push_back(root->val);
+        
+        if(sum == targetSum and root->left == NULL and root->right == NULL){
+            ans.push_back(path);
+        }
+        getPath(root->left , ans, path, sum , targetSum);
+        getPath(root->right, ans, path, sum , targetSum);
+        
+        path.pop_back();
+        sum-= root->val;
+        
     }
-
-    getPath(root->left, ans, path, sum, targetSum);
-    getPath(root->right, ans, path, sum, targetSum);
-
-    // Backtrack by removing the last element from the path and subtracting its value from sum
-    path.pop_back();
-    sum -= root->val;
-}
-
-vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-    vector<vector<int>> ans;
-    if (root == nullptr)
+    
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        int sum =0;
+        if(!root ) return ans;
+        vector<int> path;
+        getPath(root, ans,path,sum,targetSum);
         return ans;
-
-    vector<int> path;
-    int sum = 0;
-    getPath(root, ans, path, sum, targetSum);
-
-    return ans;
-}
-
+        
+    }
 };
